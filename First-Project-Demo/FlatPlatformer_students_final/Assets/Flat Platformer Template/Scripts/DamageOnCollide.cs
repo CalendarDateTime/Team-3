e3827328ae162cb1dba61f2f0250b6344d0c519d
dilarakal1 <48969671+dilarakal1@users.noreply.github.com>
2019-10-16 16:57:54 -0700

    Added the class to the function

--- First-Project-Demo/FlatPlatformer_students_final/Assets/Flat Platformer Template/Scripts/DamageOnCollide.cs
+++ First-Project-Demo/FlatPlatformer_students_final/Assets/Flat Platformer Template/Scripts/DamageOnCollide.cs
@@ -19,5 +19,33 @@ public class DamageOnCollide : MonoBehaviour
     //functions to run when object is set to destroy itself
     public UnityEvent DestroyFunctions;
 
-    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.Health -= DamageAmount;
+
+        }
+        if (DestroyOnCollide)
+        {
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.Health -= DamageAmount;
+
+        }
+        if (DestroyOnCollide)
+        {
+
+            Destroy(gameObject);
+        }
+    }
 }
